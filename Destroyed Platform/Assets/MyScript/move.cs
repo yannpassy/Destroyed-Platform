9d35010a2f0087538b5dcaafa97a496b76ecff62
Maxilag <guillaume.seyer@edu.itescia.fr>
2016-06-30 21:00:44 +0100

    changement souris

--- Destroyed Platform/Assets/MyScript/move.cs
+++ Destroyed Platform/Assets/MyScript/move.cs
@@ -4,26 +4,22 @@ using UnityEngine.VR;
 
 public class move : MonoBehaviour
 {
-    /*private Transform myTransform;            // this transform
-    private Vector3 destinationPosition;      // The destination Point*/
+    private Transform myTransform;            // this transform
     private float destinationDistance;         // The distance between myTransform and destinationPosition
-	public Vector3 positionDepart, positionArrivee, direction;
-	public GameObject perso;
+	private Vector3 nouvellePosition;
+	int mask;
 
     public float moveSpeed;      // The Speed the character will move
-	int layerMask;
     public float speed;
 
-	public Camera camera1;
+	//public Camera camera1;
 	public GameObject cube;
 
     void Start()
     {
-        /*myTransform = transform;                     // sets myTransform to this GameObject.transform
-        destinationPosition = myTransform.position;         // prevents myTransform reset*/
-		layerMask = 1 << 8;
-		perso = GameObject.Find ("perso");
-		positionArrivee = transform.position;
+        myTransform = transform;                     // sets myTransform to this GameObject.transform
+		nouvellePosition = myTransform.position;         // prevents myTransform reset*/
+		mask = (1 << 8);
 
     }
 
@@ -31,40 +27,59 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		destinationDistance = Vector3.Distance(transform.position, positionArrivee);
-		cube = GameObject.Find ("Cube");
-		//cube.transform.position = positionArrivee;
+		destinationDistance = Vector3.Distance(nouvellePosition, myTransform.position);
+		//cube = GameObject.Find ("Cube");
+		//cube.transform.position = nouvellePosition;
 
-        /*if (destinationDistance < .5f)
+        if (destinationDistance < .5f)
         {      // To prevent shakin behavior when near destination
             moveSpeed = 0;
         }
         else if (destinationDistance > .5f)
         {         // To Reset Speed to default
             moveSpeed = speed;
-        }*/
+        }
 
-    
+
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
 
-		if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-        {
-           /* Vector3 targetPoint = ray.GetPoint(hitdist);
-            destinationPosition = ray.GetPoint(hitdist);
-            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-            myTransform.rotation = targetRotation;
-			camera1.transform.LookAt (ray.GetPoint (hitdist));*/
-			positionArrivee = hit.point;
-			direction = positionArrivee - transform.position;
-        }
+		if (Physics.Raycast (ray, out hit, Mathf.Infinity, mask)) {
+			//Vector3 targetPoint = ray.GetPoint(hit);
+			//Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+			//myTransform.rotation = targetRotation;
+			//camera1.transform.LookAt (ray.GetPoint (hit));
+			Vector3 targetPoint = hit.point;
+			nouvellePosition = hit.point;
+			//Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+			Quaternion targetRotation = Quaternion.LookRotation (new Vector3 (targetPoint.x - transform.position.x, targetPoint.y + (transform.localScale.y / 2) - transform.position.y, targetPoint.z - transform.position.z));
+			myTransform.rotation = targetRotation;
+
+
+			}
+
+		/*Plane playerPlane = new Plane(Vector3.up, myTransform.position);
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		float hit = 0.0f;
+
+		if (playerPlane.Raycast(ray, out hit))
+		{
+			Vector3 targetPoint = ray.GetPoint(hit);
+			nouvellePosition = ray.GetPoint (hit);
+			Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+			myTransform.rotation = targetRotation;
+
+
+		}*/
+
+		Debug.Log (destinationDistance);
 
 
         // To prevent code from running if not needed
-        if (destinationDistance > .5)
-        {
-            //myTransform.position = Vector3.MoveTowards(myTransform.position, destinationPosition, moveSpeed * Time.deltaTime);
-			transform.position += direction * speed;
-        }
+		if (destinationDistance > 1.5f) { 
+			transform.position = Vector3.MoveTowards (myTransform.position, nouvellePosition, moveSpeed * Time.deltaTime);
+			//Debug.Log (transform.position.x + "  " + transform.position.y + "  " + transform.position.z);
+		}
+
     }
 }
