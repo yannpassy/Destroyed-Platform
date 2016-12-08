6143470b59466790872a2abce4f59c826c4ae81b
Maxilag <guillaume.seyer@edu.itescia.fr>
2016-12-08 14:33:22 +0100

    shader disolvation
    shader qui permet de desintégrer les blocs

---
+++ Destroyed Platform/Assets/ShaderActive.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderActive : MonoBehaviour {
+    public Shader shaderDisolve;
+    public Renderer rend;
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+        shaderDisolve = Shader.Find("Disolving");
+	}
+	
+	// Update is called once per frame
+	void OnColliderEnter (Collision collision) {
+		if(collision.gameObject.tag == "bloc")
+        {
+            rend.material.shader.
+        }
+	}
+}
